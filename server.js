292c6a5ae639891b023a5091fe22fc79a4826175
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-08 21:35:12 -0500

    created login logout capability

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path')
+const session = require('express-session');
+require('dotenv').config();
+
+const SequilizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.Secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequilizeStore({
+        db: sequelize
+    })
+}
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +31,7 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session(sess))
 
 // turn on routes 
 app.use(routes);
