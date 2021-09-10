e95a3ce78bb376fad0f866ee8f29996acdd00f4e
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-09 22:37:43 -0500

    asfa

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -23,7 +23,10 @@ router.get('/', (req, res) => {
             }
         ]
     })
-        .then(dbPostData => res.json(dbPostData))
+        .then(dbPostData => {
+            console.log(dbPostData)
+            res.json(dbPostData)
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
