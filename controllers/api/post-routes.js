d10672d47be30be456f7063754160fbb461e4897
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-09 20:59:01 -0500

    dashboard complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users 
 router.get('/', (req, res) => {
@@ -63,11 +64,11 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -76,7 +77,7 @@ router.post('/', (req, res) => {
     });    
 });
 
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // make sure the session exists first
     if (req.session) {
         // pass session id along with all destructured properties on req.body
@@ -92,7 +93,7 @@ router.put('/upvote', (req, res) => {
   
 });
 
-router.put('/:id', (req,res) => {
+router.put('/:id', withAuth, (req,res) => {
     Post.update(
         {
             title: req.body.title
@@ -116,7 +117,7 @@ router.put('/:id', (req,res) => {
         })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path')
 const session = require('express-session');
+const helpers = require('./utils/helpers')
 require('dotenv').config();
 
 const SequilizeStore = require('connect-session-sequelize')(session.Store);
@@ -23,7 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
