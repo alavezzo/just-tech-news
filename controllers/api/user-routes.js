5471f6807adb55ed9efcc1cd3cb29bbd84b216d9
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-10 23:43:31 -0500

    fixed user sign up bug.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -69,6 +69,8 @@ router.post('/', (req, res) => {
                 req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
                 req.session.loggedIn = true;
+
+                res.json(dbUserData)
             }))
         .catch(err => {
             console.log(err);
